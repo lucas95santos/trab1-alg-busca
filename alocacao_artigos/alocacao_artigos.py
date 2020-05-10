523ad62f37d8025c2394dcc5faa030c78adc9d67
holandah <54658970+holandah@users.noreply.github.com>
2020-05-10 01:35:24 -0400

    Add files via upload

--- alocacao_artigos/alocacao_artigos.py
+++ alocacao_artigos/alocacao_artigos.py
@@ -8,6 +8,7 @@
 
 
 import csv
+import random
 
 #arquivos
 inputpath = "./input.txt"
@@ -18,10 +19,6 @@ output = "saida-genetico.txt"
 #parametros
 
 """
-k individuos inicias gerados aleatoriamente
-seleçao
-reprodução
-mutação
 
 função de fitness: 
 -reflete quão bem adaptado é um individuo
@@ -44,7 +41,8 @@ aos pais
 """
 
 crossover_rate = 0
-mutationrate = 0
+#mutationra deve ser um valor alpha, onde 0 <= alpha <= m (numero de arquivos a ser revisados)
+mutationrate = 1
 maxgen = 100
 
 
@@ -76,24 +74,7 @@ def write_output_file(s):
     Processo de seleção por roleta
 """
 
-"""
-    Conferências cientı́ficas baseiam a seleção de artigos a serem publicados em processos de revisão
-    por pares. Nestes, cientistas proeminentes da área da conferência são convidados a contribuir,
-    revisando artigos submetidos pelos autores, indicando a aprovação ou rejeição dos mesmos.
-    
-    Um dos grandes problemas enfrentados pelos organizadores dos eventos, no entanto, consiste na
-    atribuição dos artigos aos revisores. Em geral, um processo de votação é feito antes da atribuição,
-    no qual os revisores são apresentados ao tı́tulo e ao resumo dos artigos a serem avaliados e atribuem
-    uma nota inteira de afinidade entre 0 (não desejo revisar, não conheço o tema do artigo) e 5 (desejo
-    muito revisar, sou especialista na área do artigo). Além disso, os revisores indicam quantos artigos,
-    no máximo, gostariam de revisar para este evento. O grande desafio consiste em atribuir os artigos
-    recebidos aos melhores revisores, buscando sempre respeitar os limites de cada colaborador.
-
-    Elabore uma solução de atribuição de artigos baseada em algoritmos genéticos, implementada
-    em Python, para auxiliar este processo. Sua solução deverá atribuir artigos de maneira a maximi-
-    zar a afinidade revisor / artigo da distribuição. Nenhum artigo pode ficar sem revisão e deve-se,
-    sempre, respeitar o máximo de artigos que um revisor pode receber.
-    
+"""    
     Seu algoritmo deve receber como entrada uma matriz N xM + 1, lida a partir de um arquivo
     textual. Nesta matriz, N é o número de revisores cadastrados e M é o número de artigos a serem
     atribuı́dos. Ao final de cada linha, estará expresso o máximo de artigos que o revisor aceita receber
@@ -108,52 +89,73 @@ def write_output_file(s):
     afinidade 0 com os artigos 1 e 2; afinidade 3 com o artigo 3; afinidade 4 com os artigos 4 e 5; e
     aceita receber, no máximo, 1 artigo para revisar.
     
-    Na sua solução, deve-se ler uma matriz a partir de um arquivo de entrada, no formato N xM +1,
-    como a de exemplo apresentada anteriormente. A codificação genética dos estados deve ser definida
-    por você. Você deve inicializar os indivı́duos aleatoriamente. Por isso, 10 repetições devem
-    ser feitas para um mesmo problema. Você deverá utilizar os operadores de seleção, mutação e
-    crossover. Devem ser parâmetros do seu algoritmo a taxa de crossover (com o nome crossoverrate),
-    a taxa de mutação (com o nome mutationrate), o máximo de gerações a serem desenvolvidas (com
-    o nome maxgen e valor padrão de 100) e o caminho para o arquivo de entrada da matriz (com o
-    nome inputpath). A função de fitness a ser utilizada também deve ser definida por você, devendo
-    ser aplicada para o processo de seleção por roleta.
-    
-    Seu programa deverá gerar um gráfico da evolução da função de fitness ao longo das gerações
-    (gráfico de linha, com o nome “fitness.png”). Duas linhas devem estar no gráfico, uma para a
-    melhor solução e outra para a média das 10 repetições.
-    
-    
-    Também, deve ser gerado um arquivo “saida-genetico.txt”, reportando o resultado obtido ao
-    final da execução que, dentre as 10 repetições, teve a melhor função de fitness. Nesta saı́da, uma
-    única linha de tamanho M deve ser gerada, indicando o ı́ndice do revisor atribuı́do a um determi-
-    nado artigo. Abaixo, um exemplo de saı́da para o problema de entrada reportado anteriormente.
-    
     3,2,1,4,4
     
     Neste exemplo, o artigo 1 foi atribuı́do ao revisor 3; o artigo 2 ao revisor 2; o artigo 3 ao revisor
     1; o artigo 4 ao revisor 4; e o artigo 5 também ao revisor 4.    
     
 """
+#geração da populaçao aleatoria
+def random_population(data, n, m, number_of_individuals):
+    p = [] #lista da população gerada
+    for j in range(number_of_individuals):
+        new_guy = []
+        for s in range(m):
+            while len(new_guy) < m:
+                x = random.randint(1, n)
+                if new_guy.count(x) < data[x-1][m]:
+                    new_guy.append(x)
+        p.append(new_guy)
+    return p
 
-def genetic_algotithm(data):
+#A função de fitness considera o grau de satisfação dos corretores em relação a distribuição escolhida
+def fitness(data, generation):
+    f = []
+    for k in range(len(generation)):
+        soma = 0
+        for l in range(len(generation[k])):
+            soma += data[generation[k][l]-1][l]
+        f.append(soma)
+    return f
+        
+    
+    
+                
+
+def genetic_algotithm(data, n_individuals):
     
     # n presenta o numero de revisores cadastrados
     n = len(data)
     # m numero de artigos a serem atribuidos
     m = len(data[0])-1
-    #print (n, m)    
     
+    population = random_population(data, n, m, n_individuals)
+    
+        
+    for g in range(maxgen):
+        #selecao
+        f = fitness(data, population)
+     
+        #crosover
+        
+        #mutacao
+         
+
+        
+    
+      
     
 
 if __name__ == '__main__':
     #leitura do arquivo
     data = read_input_file()
     
-    #execução do algoritmo genetico
-    #solution = genetic_algotithm(data)
-    genetic_algotithm(data)
-
-    #escrita do arquivo
-    #write_output_file(solution)
+    for i in range(10):
+        #execução do algoritmo genetico
+        #solution = genetic_algotithm(data)
+        genetic_algotithm(data, 10)
+    
+        #escrita do arquivo
+        #write_output_file(solution)
     
     
